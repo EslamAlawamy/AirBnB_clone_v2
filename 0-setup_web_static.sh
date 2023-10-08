f3f975c07fd847be5558c07ad16d72005a55eeb4
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-10-08 13:25:31 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,13 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo sh -c 'echo "this is Emad"> /data/web_static/releases/test/index.html'
+sudo sh -c 'echo "this is alawamy"> /data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sh -c 'echo "server {
     listen 80;
     listen [::]:80 default_server;
-    server_name emadanwer.tech;
+    server_name alawamy.tech;
     location /hbnb_static {
         alias /data/web_static/current/;
     }
