6169773bad6265e6ca8c5e4dfbab199ac3237f1f
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-10-22 17:18:02 +0300

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,9 +17,8 @@ def teardown(exception):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Display a list of states and their cities """
-    states =storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
